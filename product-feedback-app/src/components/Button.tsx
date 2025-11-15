a11a1452dd9966dc2121134cc7c3aa17c44eefcd
DimitarK13 <dimikalapocev@gmail.com>
2025-11-15 15:24:37 +0100

    feat(product-feedback-app/button): handle links

--- product-feedback-app/src/components/Button.tsx
+++ product-feedback-app/src/components/Button.tsx
@@ -1,15 +1,26 @@
+import { useNavigate } from 'react-router';
+
+type BtnStyle = 'primary' | 'secondary' | 'neutral' | 'action' | 'arrow' | 'arrow-neutral';
+
 interface ButtonTypes {
   text: string;
   icon?: {
     src: string;
     alt: string;
   };
-  style: string;
+  style: BtnStyle;
+  url?: string;
 }
 
-export default function Button({ text, icon, style }: ButtonTypes) {
+export default function Button({ text, icon, style, url }: ButtonTypes) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (url) navigate(url);
+  };
+
   return (
-    <button className={`btn btn--${style}`}>
+    <button className={`btn btn--${style}`} onClick={handleClick}>
       {icon && <img {...icon} />}
       {text}
     </button>
