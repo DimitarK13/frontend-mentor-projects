fee2fd20ee99f45641a7528bf4c61bb7cbbf4550
DimitarK13 <dimikalapocev@gmail.com>
2025-11-16 20:25:46 +0100

    feat(entertainment-web-app): include header in app

--- entertainment-web-app/src/App.tsx
+++ entertainment-web-app/src/App.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router';
 
+import Header from './components/header/Header';
+
 export default function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
 
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
