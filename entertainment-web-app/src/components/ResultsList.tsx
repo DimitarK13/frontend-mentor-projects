6b59f95163d0ca548d999ef37ce618a2349bbd0d
DimitarK13 <dimikalapocev@gmail.com>
2025-11-18 11:33:15 +0100

    feat(entertainment-web-app): create and style results-list component

---
+++ entertainment-web-app/src/components/ResultsList.tsx
@@ -0,0 +1,54 @@
+import Preview from './cards/Preview';
+
+type Categories = 'Movie' | 'TV Series';
+
+interface DefaultTypesItem {
+  title: string;
+  year: number;
+  category: string;
+  rating: string;
+  isBookmarked: boolean;
+  isTrending: boolean;
+  thumbnail: {
+    regular: {
+      small: string;
+      medium: string;
+      large: string;
+    };
+    trending?: {
+      small: string;
+      large: string;
+    };
+  };
+}
+
+interface DefaultTypes {
+  title: string;
+  data: DefaultTypesItem[];
+  isTrending: boolean;
+}
+
+export default function ResultsList({ title, data, isTrending }: DefaultTypes) {
+  return (
+    <section className={`results ${isTrending ? 'trending' : ''}`}>
+      <h2 className="results-header">{title}</h2>
+
+      <ul className={`results-list ${isTrending ? 'slider' : ''}`}>
+        {data.map((item) => {
+          return (
+            <Preview
+              key={item.title}
+              title={item.title}
+              year={item.year}
+              category={item.category as Categories}
+              rating={item.rating}
+              isBookmarked={item.isBookmarked}
+              isTrending={item.isTrending}
+              thumbnail={item.thumbnail.regular}
+            />
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
