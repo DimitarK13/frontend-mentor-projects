f97a0b2110e2258da2607f9eaed62f274dd534a1
DimitarK13 <dimikalapocev@gmail.com>
2025-11-18 11:33:41 +0100

    feat(entertainment-web-app): render trending and recommended lists on homepage

--- entertainment-web-app/src/routes/Home.tsx
+++ entertainment-web-app/src/routes/Home.tsx
@@ -1,3 +1,12 @@
+import ResultsList from '../components/ResultsList';
+
+import data from '../../data.json';
+
 export default function Home() {
-  return <h1>Homepage</h1>;
+  return (
+    <>
+      <ResultsList title="Trending" data={data.filter((item) => item.isTrending)} isTrending={true} />
+      <ResultsList title="Recommended for you" data={data.filter((item) => !item.isTrending)} isTrending={false} />
+    </>
+  );
 }
