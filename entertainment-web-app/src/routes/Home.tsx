52aef955a5c8c2c6717d45451d4424956ef91061
DimitarK13 <dimikalapocev@gmail.com>
2025-11-18 12:23:03 +0100

    feat(entertainment-web-app): render and handle search

--- entertainment-web-app/src/routes/Home.tsx
+++ entertainment-web-app/src/routes/Home.tsx
@@ -1,12 +1,37 @@
+import { useState } from 'react';
+
 import ResultsList from '../components/ResultsList';
 
 import data from '../../data.json';
+import Search from '../components/Search';
 
 export default function Home() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  function handleSearch(query: string) {
+    setSearchQuery(query);
+  }
+
   return (
     <>
-      <ResultsList title="Trending" data={data.filter((item) => item.isTrending)} isTrending={true} />
-      <ResultsList title="Recommended for you" data={data.filter((item) => !item.isTrending)} isTrending={false} />
+      <Search placeholder="Search for movies or TV series" onSearch={handleSearch} />
+
+      {searchQuery && (
+        <ResultsList
+          title={`Found ${
+            data.filter((item) => item.title.toLowerCase().includes(searchQuery.toLowerCase())).length
+          } results for  '${searchQuery}'`}
+          data={data.filter((item) => item.title.toLowerCase().includes(searchQuery.toLowerCase()))}
+          isTrending={false}
+        />
+      )}
+
+      {!searchQuery && (
+        <>
+          <ResultsList title="Trending" data={data.filter((item) => item.isTrending)} isTrending={true} />
+          <ResultsList title="Recommended for you" data={data.filter((item) => !item.isTrending)} isTrending={false} />
+        </>
+      )}
     </>
   );
 }
